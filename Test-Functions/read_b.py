979e596019f24d9d2e2957f0bfb484378c3fed7a
rmason200 <62139868+rmason200@users.noreply.github.com>
2020-05-11 15:03:28 +0100

    Delete read_b.py

--- Test-Functions/read_b.py
+++
@@ -1,8 +0,0 @@
-# for the Adafruit IO API
-from Adafruit_IO import Client
-
-aio = Client("<USERNAME>", "<API-KEY>")
-
-# receives status of signal from API, prints to terminal
-UnitB_status = aio.receive('status-b').value
-print("Unit B Signal State: " + str(UnitB_status))
